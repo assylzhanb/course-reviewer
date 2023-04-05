22a122c4ad7c9cb80aa49e2d981dd828e8410f6c
assylzhanb <asylzhan127@gmail.com>
2023-04-05 20:14:28 +0900

    changes

--- submit/run.sh
+++ submit/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 git clone https://github.com/assylzhanb/course-reviewer.git
 cd course-reviewer
-mvn install
-java -jar target/cse364-project-1.0-SNAPSHOT-jar-with-dependencies.jar
+mvn spring-boot:run
+# mvn install
+# java -jar target/cse364-project-1.0-SNAPSHOT-jar-with-dependencies.jar
