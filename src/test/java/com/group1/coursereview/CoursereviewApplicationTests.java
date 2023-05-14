da64a1761178dd990f484ca8cb48ae711ea16817
assylzhanb <asylzhan127@gmail.com>
2023-05-15 08:56:20 +0900

    tests checked

--- src/test/java/com/group1/coursereview/CoursereviewApplicationTests.java
+++ src/test/java/com/group1/coursereview/CoursereviewApplicationTests.java
@@ -3,7 +3,7 @@ package com.group1.coursereview;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = CoursereviewApplication.class)
 class CoursereviewApplicationTests {
 
 	@Test
