ae36a4e1255adcc7d40294bbac2285c2a6dcef14
assylzhanb <asylzhan127@gmail.com>
2023-05-15 20:42:09 +0900

    submit updated

--- submit/run.sh
+++ submit/run.sh
@@ -2,6 +2,7 @@
 git clone https://github.com/assylzhanb/course-reviewer.git
 cd course-reviewer
 git checkout milestone2
+mvn clean install jacoco:report
 mvn spring-boot:run
 # mvn install
 # java -jar target/cse364-project-1.0-SNAPSHOT-jar-with-dependencies.jar
