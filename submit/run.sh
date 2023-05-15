12546cd47ccd4daaa369f89773cfb1eafe47543f
Myrat Bayramov <myratbayramov@unist.ac.kr>
2023-05-15 19:52:04 +0900

    added git checkout to run.sh

--- submit/run.sh
+++ submit/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 git clone https://github.com/assylzhanb/course-reviewer.git
 cd course-reviewer
+git checkout milestone2
 mvn spring-boot:run
 # mvn install
 # java -jar target/cse364-project-1.0-SNAPSHOT-jar-with-dependencies.jar
